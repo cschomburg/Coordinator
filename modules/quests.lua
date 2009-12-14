724ef84b7c2ac675e9748afa0e650303660b818e
xconstruct <xconstruct@gmail.com>
2009-12-14 16:47:54 +0100

    alpha quest-module included

---
+++ modules/quests.lua
@@ -0,0 +1,17 @@
+local module = CreateFrame("Frame")
+
+local target
+
+module:SetScript("OnUpdate", function()
+	local questID = WorldMapQuestScrollChildFrame.selected and WorldMapQuestScrollChildFrame.selected.questId
+	if(not questID) then return target and Coordinator.DisableOverlay(target) end
+
+	local _, posX, posY= QuestPOIGetIconInfo(questID)
+	if(not posX or not posY) then return target and Coordinator.DisableOverlay(target) end
+
+	if(not target) then
+		target = Coordinator.DisplayCoordinate("Quest", posX, posY)
+	else
+		target.x, target.y = posX, posY
+	end
+end)
